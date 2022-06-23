67bd39a68ecb97ab46250b5295120275f5a609d5
zejun19 <wangzejun19@outlook.com>
2022-06-23 20:13:44 +0800

    test: add pageable query test case

--- src/test/java/org/openeuler/sbom/manager/UserTests.java
+++ src/test/java/org/openeuler/sbom/manager/UserTests.java
@@ -1,12 +1,14 @@
 package org.openeuler.sbom.manager;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openeuler.sbom.manager.model.PageVo;
 import org.openeuler.sbom.manager.model.UserEntity;
 import org.openeuler.sbom.manager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +40,7 @@ public class UserTests {
 
     @Test
     @Order(1)
-    public void removeAllRecord() throws Exception {
+    public void removeAllRecord() {
         userService.deleteAllUsers();
     }
 
@@ -54,18 +56,15 @@ public class UserTests {
     @Test
     @Order(3)
     public void addUser() throws Exception {
-        UserEntity user = new UserEntity();
-        user.setUserName("First");
-        user.setEmail("first@someemail.com");
-
-        this.mockMvc.perform(post("/sbom/addUser")
+        this.mockMvc
+                .perform(post("/sbom/addUser")
                         .param("name", "First")
                         .param("email", "first@someemail.com")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk()).
-                andExpect(content().string("Saved"));
+                .andExpect(status().isOk())
+                .andExpect(content().string("Saved"));
     }
 
     @Test
@@ -74,7 +73,8 @@ public class UserTests {
         UserEntity user = new UserEntity();
         user.setUserName("Second");
         user.setEmail("second@someemail.com");
-        this.mockMvc.perform(post("/sbom/addUserRecord")
+        this.mockMvc
+                .perform(post("/sbom/addUserRecord")
                         .content(new ObjectMapper().writeValueAsString(user))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
@@ -83,23 +83,34 @@ public class UserTests {
                 .andExpect(content().string("Saved"));
     }
 
+    @Test
+    @Order(4)
+    public void addUserRecords() {
+        for (int i = 0; i < 10; i++) {
+            UserEntity user = new UserEntity();
+            user.setUserName("Third");
+            user.setEmail("third_" + i + "@someemail.com");
+            userService.addNewUserByEntity(user);
+        }
+    }
+
     @Test()
     @Order(5)
     public void findAllRecordAfterAdd() throws Exception {
-        String responseJsonString = this.mockMvc.perform(get("/sbom/allUser"))
+        String responseJsonString = this.mockMvc
+                .perform(get("/sbom/allUser"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.*", hasSize(2)))
+                .andExpect(jsonPath("$.*", hasSize(12)))
                 .andExpect(jsonPath("$.[0].userName").value("First"))
                 .andReturn().getResponse().getContentAsString();
 
         ObjectMapper objectMapper = new ObjectMapper();
-        List<UserEntity> users = objectMapper.readValue(responseJsonString, new TypeReference<List<UserEntity>>() {
+        List<UserEntity> users = objectMapper.readValue(responseJsonString, new TypeReference<>() {
         });
 
         Assert.notEmpty(users, "users list is empty");
     }
-
 
     @Test()
     @Order(6)
@@ -107,6 +118,26 @@ public class UserTests {
         Assert.isTrue(!CollectionUtils.isEmpty(userService.findByUserName1("First")), "can`t find First");
         Assert.isTrue(!CollectionUtils.isEmpty(userService.findByUserName2("Second")), "can`t find Second 2");
         Assert.isTrue(!CollectionUtils.isEmpty(userService.findByUserName3("Second")), "can`t find Second 3");
+    }
+
+    @Test()
+    @Order(7)
+    public void findAllPageable() {
+        PageVo<UserEntity> pageResult = userService.findAllPageable(2, 2);
+
+        Assert.isTrue(pageResult.getTotalElements() == 12, "total size error");
+        Assert.isTrue(pageResult.getSize() == 2, "page size error");
+        Assert.isTrue("Third".equals(pageResult.getContent().get(1).getUserName()), "second record`s name error");
+    }
+
+    @Test()
+    @Order(8)
+    public void findAllPageableByUserName() {
+        PageVo<UserEntity> pageResult = userService.findAllPageable("Third", 2, 2);
+
+        Assert.isTrue(pageResult.getTotalElements() == 10, "total size error");
+        Assert.isTrue(pageResult.getSize() == 2, "page size error");
+        Assert.isTrue("Third".equals(pageResult.getContent().get(1).getUserName()), "second record`s name error");
     }
 }
 
