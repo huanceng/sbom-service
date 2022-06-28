5bd2d94e625e1d24775ffe8a1daa0fc380d2d32b
zejun19 <wangzejun19@outlook.com>
2022-06-28 10:58:43 +0800

    feat: add RawSbom custom query method

--- src/main/java/org/openeuler/sbom/manager/dao/RawSbomRepository.java
+++ src/main/java/org/openeuler/sbom/manager/dao/RawSbomRepository.java
@@ -2,8 +2,15 @@ package org.openeuler.sbom.manager.dao;
 
 import org.openeuler.sbom.manager.model.RawSbom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface RawSbomRepository extends JpaRepository<RawSbom, UUID> {
+
+    @Query(value = "select * from raw_sbom where product_name = :#{#rawSbom.productName} and format = :#{#rawSbom.format}" +
+                    " and spec = :#{#rawSbom.spec} and spec_version = :#{#rawSbom.specVersion}",
+            nativeQuery = true)
+    RawSbom queryRawSbom(RawSbom rawSbom);
+
 }
