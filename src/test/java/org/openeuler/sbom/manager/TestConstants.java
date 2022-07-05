3f015a25df806b626bc6a0b6ffca4ec9a54f8a08
zejun19 <wangzejun19@outlook.com>
2022-07-05 15:16:09 +0800

    test: remove demo code

--- src/test/java/org/openeuler/sbom/manager/TestConstants.java
+++ src/test/java/org/openeuler/sbom/manager/TestConstants.java
@@ -4,8 +4,6 @@ public class TestConstants {
 
     public static final String SAMPLE_UPLOAD_FILE_NAME = "sample/sample-spdx.json";
 
-    public static final String SAMPLE_UPLOAD_COPY_FILE_NAME = "sample/sample-spdx-copy.json";
-
     public static final String SAMPLE_PRODUCT_NAME = "mindsporeTest";
 
     public static final String OPENEULER_PRODUCT_NAME = "openEuler-22.03-LTS-x86_64-everything";
