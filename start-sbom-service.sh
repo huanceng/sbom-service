7b473b9970f9c8a969aa473ca2521cfa548c6278
huanceng <10809369+huanceng@user.noreply.gitee.com>
2022-07-07 17:01:14 +0800

    Feat: docker deployment

--- start-sbom-service.sh
+++ start-sbom-service.sh
@@ -2,14 +2,11 @@
 
 set -e
 
+export DB_PASSWORD=$(cat ${DB_PASSWORD_FILE})
+
 WORKSPACE=/opt/sbom-service
 
 cd ${WORKSPACE}
-
-APP_PROPERTIES_FILE=${WORKSPACE}/src/main/resources/application.properties
-sed -i "s/{db_url}/jdbc\:postgresql\:\/\/${DB_HOST}\:${DB_PORT}\/${DB_NAME}/" ${APP_PROPERTIES_FILE}
-sed -i "s/{db_username}/${DB_USERNAME}/" ${APP_PROPERTIES_FILE}
-sed -i "s/{db_password}/$(cat ${DB_PASSWORD_FILE})/" ${APP_PROPERTIES_FILE}
 
 /bin/bash gradlew bootWar
 
