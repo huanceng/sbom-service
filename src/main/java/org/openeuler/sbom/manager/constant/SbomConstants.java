f6d0aaa4416f5d6b0d561f19a27b5667f42ff181
zejun19 <wangzejun19@outlook.com>
2022-07-28 15:42:32 +0800

    feat: add CORS config to octet-stream api

--- src/main/java/org/openeuler/sbom/manager/constant/SbomConstants.java
+++ src/main/java/org/openeuler/sbom/manager/constant/SbomConstants.java
@@ -1,5 +1,8 @@
 package org.openeuler.sbom.manager.constant;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SbomConstants {
     public static final String READER_NAME = "reader";
     public static final String WRITER_NAME = "writer";
@@ -9,5 +12,7 @@ public class SbomConstants {
     public static final String SWID_NAME = "SWID";
 
     public static final int MAX_QUERY_LINE = 15;
+
+    public static final List<String> ALLOW_ORIGINS= Arrays.asList("http://localhost:8080","http://127.0.0.1:8080");
 
 }
