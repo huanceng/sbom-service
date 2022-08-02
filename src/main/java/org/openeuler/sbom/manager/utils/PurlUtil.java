889398f3629853962c314f79bf149319f2e2206f
zejun19 <wangzejun19@outlook.com>
2022-08-02 10:36:44 +0800

    feat: purl package manager support : rpm\pypi\gitee\github\gitlab

--- src/main/java/org/openeuler/sbom/manager/utils/PurlUtil.java
+++ src/main/java/org/openeuler/sbom/manager/utils/PurlUtil.java
@@ -3,6 +3,7 @@ package org.openeuler.sbom.manager.utils;
 import com.github.packageurl.MalformedPackageURLException;
 import com.github.packageurl.PackageURL;
 import org.apache.commons.lang3.StringUtils;
+import org.openeuler.sbom.manager.constant.SbomConstants;
 import org.openeuler.sbom.manager.model.vo.PackageUrlVo;
 import org.springframework.data.util.Pair;
 
@@ -24,6 +25,13 @@ public class PurlUtil {
         switch (type) {
             case "maven":
                 return PurlUtil.generateMavenPurlQueryCondition(purl.getNamespace(), purl.getName(), purl.getVersion());
+            case "rpm":
+                return PurlUtil.generateRpmPurlQueryCondition(type, purl.getName(), purl.getVersion());
+            case "pypi":
+            case "github":
+            case "gitlab":
+            case "gitee":
+                return PurlUtil.generateNoNamespacePurlQueryCondition(type, purl.getName(), purl.getVersion());
             // TODO 后续追加其他包管理的支持
         }
 
@@ -39,9 +47,36 @@ public class PurlUtil {
             PackageURL purl = new PackageURL(type, namespace, name, null, null, null);
             return Pair.of(purl.getCoordinates(), false);
         } else if (StringUtils.isNotEmpty(name)) {
-            return Pair.of(name, false);
+            return Pair.of(SbomConstants.PURL_SCHEMA_DEFAULT + ":" + type + "%" + name, false);
         } else {
             throw new RuntimeException("maven purl query condition params is error, namespace: %s, name: %s, version: %s".formatted(namespace, name, version));
+        }
+    }
+
+    /**
+     * RPM PURL使用name+version
+     * <p>
+     * 但是RPM的version比较特殊，使用epoch:version-release格式，转成PURL之后，PURL中的version为version-release；epoch放在PURL的qualifiers中，<a href="https://github.com/package-url/purl-spec/issues/69">https://github.com/package-url/purl-spec/issues/69</a>
+     */
+    private static Pair<String, Boolean> generateRpmPurlQueryCondition(String type, String name, String version) throws MalformedPackageURLException {
+        if (StringUtils.isNotEmpty(name) && StringUtils.isNotEmpty(version)) {
+            PackageURL purl = new PackageURL(type, null, name, version, null, null);
+            return Pair.of(purl.getCoordinates(), false);
+        } else if (StringUtils.isNotEmpty(name)) {
+            return Pair.of(SbomConstants.PURL_SCHEMA_DEFAULT + ":" + type + "%" + name, false);
+        } else {
+            throw new RuntimeException("maven purl query condition params is error, type: %s, name: %s, version: %s".formatted(type, name, version));
+        }
+    }
+
+    private static Pair<String, Boolean> generateNoNamespacePurlQueryCondition(String type, String name, String version) throws MalformedPackageURLException {
+        if (StringUtils.isNotEmpty(name) && StringUtils.isNotEmpty(version)) {
+            PackageURL purl = new PackageURL(type, null, name, version, null, null);
+            return Pair.of(purl.getCoordinates(), true);
+        } else if (StringUtils.isNotEmpty(name)) {
+            return Pair.of(SbomConstants.PURL_SCHEMA_DEFAULT + ":" + type + "%" + name, false);
+        } else {
+            throw new RuntimeException("maven purl query condition params is error, type: %s, name: %s, version: %s".formatted(type, name, version));
         }
     }
 }
